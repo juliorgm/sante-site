3390cd364330eac4fcb34352d2fca5932f7e774c
juliorgm <juliorgm@gmail.com>
2026-08-05 00:02:13 -0300

    fix: update schedule feat: add tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,15 +46,15 @@ function SchemaMarkup() {
     openingHoursSpecification: [
       {
         '@type': 'OpeningHoursSpecification',
-        dayOfWeek: ['Monday','Tuesday','Wednesday','Thursday','Friday'],
-        opens: '08:00',
-        closes: '18:00',
+        dayOfWeek: ['Monday','Tuesday','Wednesday','Thursday']
+        opens: '06:30',
+        closes: '20:30',
       },
       {
         '@type': 'OpeningHoursSpecification',
-        dayOfWeek: ['Saturday'],
-        opens: '08:00',
-        closes: '12:00',
+        dayOfWeek: ['Friday'],
+        opens: '06:30',
+        closes: '12:30',
       },
     ],
     medicalSpecialty: 'PhysicalTherapy',
@@ -73,8 +73,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="pt-BR">
       <head>
         <SchemaMarkup />
+        {/* Google Tag Manager (head) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P9CC938Z');`,
+          }}
+        />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9CC938Z" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
         <Header />
         <main>{children}</main>
         <Footer />
